f4ea840dd514a8b0e298eb0296cbd035cb80ee2d
JunYoungLee260 <jylee5847@gmail.com>
2026-03-31 01:13:35 +0900

    up

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -37,9 +37,3 @@ variable "private_dns_zone_name" {
   type        = string
   default     = "internal.aiops"
 }
-
-variable "github_dispatch_token" {
-  description = "GitHub PAT for workflow dispatch"
-  type        = string
-  sensitive   = true
-}
